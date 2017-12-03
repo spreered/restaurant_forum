de5ecbec6ddf25d9cf2eff1efb717594b50e32b5
spreered <spreered@gmail.com>
2017-12-03 22:27:57 +0800

    add restaurant index action and view for admin

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,4 @@
+class Admin::RestaurantsController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
 
+  namespace :admin do 
+    root "restaurants#index"
+  end
 end
