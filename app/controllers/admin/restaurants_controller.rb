57d9bbd9d5110ece5988fd3bdfb5ada1c52bcd57
spreered <spreered@gmail.com>
2017-12-04 14:35:57 +0800

    add restaurant index action and view for admin

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -2,6 +2,7 @@ class Admin::RestaurantsController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
   def index
+    @restaurants = Restaurant.all
   end
 
   private
