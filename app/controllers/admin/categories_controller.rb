6a4cc11c4bc7cd7502b7f922d8cf93c5f348e50e
spreered <spreered@gmail.com>
2017-12-06 16:05:06 +0800

    add category index page for admin

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,19 @@
 class Admin::CategoriesController < ApplicationController
   before_action :authenticate_user!
   before_action :authenticate_admin
+
+  def index
+    @categories = Category.all
+  end
+
+
+  private
+  
+  def authenticate_admin
+    unless current_user.admin?
+     flash[:alert] = "Not allow"
+     redirect_to root_path
+    end
+  end
+
 end
