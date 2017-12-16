4c357106841cad7be3bf10a87cf4d9c5ada9f3bf
spreered <spreered@gmail.com>
2017-12-17 00:47:41 +0800

    setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "restaurants#index"
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   resources :categories, only: [:show]
 
   namespace :admin do 
