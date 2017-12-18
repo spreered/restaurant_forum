235c431af88f96bce3160232226d687790678ea3
spreered <spreered@gmail.com>
2017-12-19 00:25:27 +0800

    admin can delete comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,16 @@ class CommentsController < ApplicationController
     @comment.save!
     redirect_to restaurant_path(@restaurant)
   end
+
+  def destroy
+    restaurant = Restaurant.find(params[:restaurant_id])
+    comment = Comment.find(params[:id])
+    if current_user.admin?
+      comment.destroy
+      redirect_to restaurant_path(restaurant)
+    end
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:content)
