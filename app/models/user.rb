f71778cab5ae78c836a638d2b95538ab953ae645
spreered <spreered@gmail.com>
2017-12-24 23:51:43 +0800

    fix some typo in user avatar uploader and restauran list

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  mount_uploader :avatar, UploaderAvatar
+  mount_uploader :avatar, AvatarUploader
   has_many :comments
   def admin?
     self.role == "admin"
