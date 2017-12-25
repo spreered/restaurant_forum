06a2cf802509aee2708acdfe68a334340f26290d
spreered <spreered@gmail.com>
2017-12-25 23:53:20 +0800

    modify user model: set default name value

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  before_save :default_name
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   mount_uploader :avatar, AvatarUploader
+
   has_many :comments
   def admin?
     self.role == "admin"
   end
+  def default_name
+    if self.name==""
+      self.name = self.email.split('@')[0]
+    end
+  end
+
 end
