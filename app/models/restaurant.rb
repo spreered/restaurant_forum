8d934435a7714e800b1bdfc8222c1479282ade3c
spreered <spreered@gmail.com>
2017-12-26 17:25:40 +0800

    modify restaurant.category.name : set to category_name and allow nil, prevent category nil error

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
   mount_uploader :image, PhotoUploader
   validates_presence_of :name
+  delegate :name,to: :category,prefix: true, allow_nil: true
 
   belongs_to :category , optional: true 
   has_many :comments
