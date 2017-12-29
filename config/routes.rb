1249e5d5b753a4428581de30bb74c8868af068ca
spreered <spreered@gmail.com>
2017-12-30 00:44:24 +0800

    setup route: like_restaurant/ unlike_restaurant

--- config/routes.rb
+++ config/routes.rb
@@ -15,8 +15,10 @@ Rails.application.routes.draw do
       get :dashboard
       post :favorite
       post :unfavorite
+      post :like
+      post :unlike
     end
-    
+
   end
 
   resources :users, only: [:show,:edit,:update]
