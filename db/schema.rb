dfd4e820d350dfe3d4d89d30bc9ce68179771b90
spreered <spreered@gmail.com>
2017-12-30 00:35:06 +0800

    generate Like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171229040216) do
+ActiveRecord::Schema.define(version: 20171229163106) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,13 @@ ActiveRecord::Schema.define(version: 20171229040216) do
   end
 
   create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.integer "user_id"
     t.integer "restaurant_id"
     t.datetime "created_at", null: false
