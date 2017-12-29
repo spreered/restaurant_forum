220aff9808d70b8d744e35dd7ceac22dc851f835
spreered <spreered@gmail.com>
2017-12-30 00:25:42 +0800

    add favorite / unfavorite button on index , restaurant#show

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,4 +7,8 @@ class Restaurant < ApplicationRecord
   has_many :comments , dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :favorited_users, through: :favorites, source: :user
+
+  def is_favorited?(user)
+    self.favorited_users.include?(user)
+  end
 end
