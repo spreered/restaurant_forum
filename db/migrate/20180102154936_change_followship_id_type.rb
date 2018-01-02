bb27a5ff2daf20197e08b5f7985b834d6a55cdf1
spreered <spreered@gmail.com>
2018-01-03 00:48:24 +0800

    fix bug: followingship model type

--- db/migrate/20180102154936_change_followship_id_type.rb
+++
@@ -1,6 +0,0 @@
-class ChangeFollowshipIdType < ActiveRecord::Migration[5.1]
-  def change
-    change_column :followships, :user_id, :integer
-    change_column :followships, :following_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102154936) do
+ActiveRecord::Schema.define(version: 20171231145130) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
