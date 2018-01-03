b028ac57ccad47cc33ba83448acf1c87af35ec15
spreered <spreered@gmail.com>
2018-01-04 00:40:14 +0800

    add friendship l2: add action friendships#create, friendships#destroy

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,22 @@
 class FriendshipsController < ApplicationController
+  def create
+    @friendship = current_user.friendships.build(addfriend_id: params[:addfriend_id])
+    if @friendship.save
+      flash[:notice] = "send a friend request"
+    else
+      flash[:alert] = @friendship.errors.full_messages.to_sentence
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @friendships = current_user.friendships.where(addfriend_id:params[:id])
+    if @friendships.length > 0
+      @friendships.destroy_all
+      flash[:alert] = "Unfriend!"
+    else
+      flash[:alert] = "醒醒吧你沒朋友"
+    end
+    redirect_back(fallback_location: root_path)
+  end
 end
