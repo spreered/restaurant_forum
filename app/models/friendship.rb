33d1422efcc4748381aa4581458c10f86112de41
spreered <spreered@gmail.com>
2018-01-04 00:35:07 +0800

    add friendship l2: fix typo in model friendship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,5 +1,5 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :addfriend, class_name: "User"
-  validates :addfirend_id, uniqueness: {scope: :user_id}
+  validates :addfriend_id, uniqueness: {scope: :user_id}
 end
